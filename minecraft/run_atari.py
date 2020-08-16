f18d9be921621922c56ce24727cc27ffea6e78dc
Adrian-Cristian BATRIN <cristianbatrin@gmail.com>
2020-08-16 22:59:24 +0300

    Switched pogo to v4

--- minecraft/run_atari.py
+++ minecraft/run_atari.py
@@ -6,28 +6,38 @@ if __name__ == '__main__':
     # environment
     parser.add_argument('--num_buffer_frames', type=int, default=4)
     parser.add_argument('--max_repeats', type=int, default=0)
-    parser.add_argument('--wrappers', choices=['deepmind', 'universe'], default='deepmind')
+    parser.add_argument(
+        '--wrappers', choices=['deepmind', 'universe'], default='deepmind')
     # model
     parser.add_argument('--policy', choices=['cnn', 'rnn'], default='rnn')
-    parser.add_argument('--cnn_architecture', choices=['deepmind', 'alec', 'openai', 'homogeneous'], default='deepmind')
+    parser.add_argument(
+        '--cnn_architecture', choices=['deepmind', 'alec', 'openai', 'homogeneous'], default='deepmind')
     parser.add_argument('--cnn_layers', type=int, default=2)
     parser.add_argument('--cnn_filters', type=int, default=64)
     parser.add_argument('--cnn_kernel_size', type=int, default=5)
     parser.add_argument('--cnn_kernel_step', type=int, default=2)
     parser.add_argument('--rnn_layers', type=int, default=1)
     parser.add_argument('--rnn_size', type=int, default=32)
-    parser.add_argument('--rnn_type', choices=['lstm', 'gru', 'simple'], default='lstm')
+    parser.add_argument(
+        '--rnn_type', choices=['lstm', 'gru', 'simple'], default='lstm')
     parser.add_argument('--fc_layers', type=int, default=0)
     parser.add_argument('--fc_size', type=int, default=32)
-    parser.add_argument('--cnn_init', choices=['orthogonal', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform'], default='glorot_normal')
-    parser.add_argument('--fc_init', choices=['orthogonal', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform'], default='glorot_normal')
-    parser.add_argument('--action_init', choices=['orthogonal', 'normal', 'uniform'], default='orthogonal')
+    parser.add_argument('--cnn_init', choices=['orthogonal', 'glorot_normal',
+                                               'glorot_uniform', 'he_normal', 'he_uniform'], default='glorot_normal')
+    parser.add_argument('--fc_init', choices=['orthogonal', 'glorot_normal',
+                                              'glorot_uniform', 'he_normal', 'he_uniform'], default='glorot_normal')
+    parser.add_argument(
+        '--action_init', choices=['orthogonal', 'normal', 'uniform'], default='orthogonal')
     parser.add_argument('--action_init_scale', type=float, default=0.01)
-    parser.add_argument('--activation', choices=['tanh', 'relu', 'elu'], default='relu')
-    parser.add_argument('--stochastic', '-s', action='store_true', default=True)
-    parser.add_argument('--deterministic', '-d', action='store_false', dest='stochastic')
+    parser.add_argument(
+        '--activation', choices=['tanh', 'relu', 'elu'], default='relu')
+    parser.add_argument('--stochastic', '-s',
+                        action='store_true', default=True)
+    parser.add_argument('--deterministic', '-d',
+                        action='store_false', dest='stochastic')
     # optimization
-    parser.add_argument('--optimizer', choices=['adam', 'rmsprop', 'nadam'], default='adam')
+    parser.add_argument(
+        '--optimizer', choices=['adam', 'rmsprop', 'nadam'], default='adam')
     parser.add_argument('--optimizer_lr', type=float, default=0.001)
     parser.add_argument('--clipnorm', type=float, default=40.0)
     parser.add_argument('--gamma', type=float, default=0.99)
@@ -35,21 +45,29 @@ if __name__ == '__main__':
     parser.add_argument('--clip_param', type=float, default=0.2)
     parser.add_argument('--initial_std', type=float, default=0.5)
     parser.add_argument('--l2_reg', type=float, default=0)
-    parser.add_argument('--policy_loss', choices=['pg', 'pposgd'], default='pposgd')
+    parser.add_argument(
+        '--policy_loss', choices=['pg', 'pposgd'], default='pposgd')
     parser.add_argument('--entropy_coef', type=float, default=0.01)
     parser.add_argument('--kld_coef', type=float, default=0.01)
     parser.add_argument('--value_coef', type=float, default=1.0)
-    parser.add_argument('--normalize_observations', action='store_true', default=False)
-    parser.add_argument('--no_normalize_observations', action='store_false', dest='normalize_observations')
-    parser.add_argument('--normalize_advantage', action='store_true', default=True)
-    parser.add_argument('--no_normalize_advantage', action='store_false', dest='normalize_advantage')
-    parser.add_argument('--normalize_baseline', action='store_true', default=True)
-    parser.add_argument('--no_normalize_baseline', action='store_false', dest='normalize_baseline')
+    parser.add_argument('--normalize_observations',
+                        action='store_true', default=False)
+    parser.add_argument('--no_normalize_observations',
+                        action='store_false', dest='normalize_observations')
+    parser.add_argument('--normalize_advantage',
+                        action='store_true', default=True)
+    parser.add_argument('--no_normalize_advantage',
+                        action='store_false', dest='normalize_advantage')
+    parser.add_argument('--normalize_baseline',
+                        action='store_true', default=True)
+    parser.add_argument('--no_normalize_baseline',
+                        action='store_false', dest='normalize_baseline')
     parser.add_argument('--repeat_updates', type=int, default=4)
     parser.add_argument('--num_local_steps', type=int, default=100)
     parser.add_argument('--adapt_kl', type=float, default=0)
     # parallelization
-    parser.add_argument('--trainer', choices=['batched', 'batched_predictor', 'batched_profiler'], default='batched')
+    parser.add_argument(
+        '--trainer', choices=['batched', 'batched_predictor', 'batched_profiler'], default='batched')
     parser.add_argument('--num_runners', type=int, default=16)
     parser.add_argument('--queue_length', type=int, default=1)
     parser.add_argument('--queue_timeout', type=float, default=None)
@@ -67,8 +85,9 @@ if __name__ == '__main__':
     parser.add_argument('--logdir', default="logs/atari")
     parser.add_argument('--csv_file')
     parser.add_argument('--load_weights')
-    parser.add_argument('--weights_by_name', action='store_true', default=False)
-    parser.add_argument("--environment", "-e", default="PongNoFrameskip-v3")
+    parser.add_argument('--weights_by_name',
+                        action='store_true', default=False)
+    parser.add_argument("--environment", "-e", default="PongNoFrameskip-v4")
 
     subparsers = parser.add_subparsers(dest='command')
     train_parser = subparsers.add_parser('train')
@@ -101,8 +120,10 @@ if __name__ == '__main__':
     trainer = trainer_class(create_env, policy_class, args)
 
     if args.command == 'train':
-        trainer.run(args.environment, args.num_timesteps, os.path.join(args.logdir, args.label))
+        trainer.run(args.environment, args.num_timesteps,
+                    os.path.join(args.logdir, args.label))
     elif args.command == 'eval':
-        trainer.eval(args.environment, args.num_timesteps, os.path.join(args.logdir, args.label))
+        trainer.eval(args.environment, args.num_timesteps,
+                     os.path.join(args.logdir, args.label))
     else:
         parser.print_usage()
